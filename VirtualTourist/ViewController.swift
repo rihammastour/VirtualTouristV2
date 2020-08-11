2cb8ccfbe27095583c7321bd31bd818881d4aa4e
rihammastour <48338432+rihammastour@users.noreply.github.com>
2020-08-11 23:53:33 +0300

    Initial Commit

---
+++ VirtualTourist/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VirtualTourist
+//
+//  Created by Riham Mastour on 21/12/1441 AH.
+//  Copyright © 1441 Riham Mastour. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
